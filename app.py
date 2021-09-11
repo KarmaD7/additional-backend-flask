175cdbe8ddd0ef199a4d1bc922c9c3874ed52040
丁韶峰 <thudsf19@gmail.com>
2021-09-11 20:31:08 +0800

    final

--- app.py
+++ app.py
@@ -10,28 +10,36 @@ DIM = 300
 model = KeyedVectors.load_word2vec_format('entity/chinese.txt')
 vocab = model.vocab
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 CORS(app, supports_credentials=True)
 
 @app.route('/recommend', methods = ['POST'])
 def recommend():
   entities = request.json
   entities = entities.get('entities')
+  if entities == None:
+    averaged_vector = np.random.rand(DIM)
+    similar = model.most_similar([averaged_vector])
+    result = [entity[0] for entity in similar]
+    result = [entity for entity in result if entity not in entities]
+    res = {'success': True, "result": result }
   averaged_vector = np.zeros(DIM)
   cnt = 0
 
   for entity in entities:
-    if entity not in app.vocab:
+    if entity not in vocab:
       continue
     cnt += 1
     averaged_vector += model[entity]
   
   if cnt == 0:
-    averaged_vector = np.random(DIM)
+    averaged_vector = np.random.rand(DIM)
   else:
     averaged_vector = averaged_vector / cnt
 
   similar = model.most_similar([averaged_vector])
-  result = [entity for entity in similar.keys()]
+  result = [entity[0] for entity in similar]
+  result = [entity for entity in result if entity not in entities]
   res = {'success': True, "result": result }
 
   return jsonify(res), 200
